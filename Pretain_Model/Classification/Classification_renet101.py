5d7dee0365ab6efd1bc44447db00081b42d740fe
Joechen0130 <piercejoe0130@gmail.com>
2021-07-25 13:44:47 +0800

    7/25

--- Pretain_Model/Classification/Classification_renet101.py
+++ Pretain_Model/Classification/Classification_renet101.py
@@ -19,7 +19,7 @@ prepocess = transforms.Compose([
     )
 ])
 
-image = Image.open("Cat.jpg")
+image = Image.open("cat.jpeg")
 image.show()
 image_preprocess = prepocess(image)
 #print(image_preprocess.shape)#[3,224,224]
